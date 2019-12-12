4a73677894e7750a8d1a233f4049ff3fbde38883
fdubois <&&yelamoko2019>
2019-12-12 15:37:40 +0100

    Add new class in domain package

---
+++ src/main/java/com/devhome/domain/Option.java
@@ -0,0 +1,19 @@
+package com.devhome.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "option")
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_id")
+    private Long id;
+
+    @Column(name = "option_value")
+    private String value;
+}
