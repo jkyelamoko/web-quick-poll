19f60d91aaae8e7c68d113c2191d9882204b15f2
fdubois <&&yelamoko2019>
2019-12-13 17:20:14 +0100

    [FEAT]: "Update ontroller Poll"

--- src/main/java/com/devhome/controller/PollController.java
+++ src/main/java/com/devhome/controller/PollController.java
@@ -3,11 +3,13 @@ package com.devhome.controller;
 import com.devhome.domain.Poll;
 import com.devhome.repository.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api")
@@ -20,5 +22,38 @@ public class PollController {
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
         Iterable<Poll> allPolls = pollRepository.findAll();
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
+
+    @PostMapping("/polls")
+    public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+        poll = pollRepository.save(poll);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/[id}")
+                .buildAndExpand(poll.getId())
+                .toUri();
+
+        responseHeaders.setLocation(newPollUri);
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/polls/{pollId}")
+    public ResponseEntity<?> getPoll(@PathVariable Long pollId){
+        Poll p = pollRepository.findOne(pollId);
+        return new ResponseEntity<>(p, HttpStatus.OK);
+    }
+
+    @PutMapping("/polls/{pollId}")
+    public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
+        Poll p = pollRepository.save(poll);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/polls/{pollId}")
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        pollRepository.delete(pollId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
